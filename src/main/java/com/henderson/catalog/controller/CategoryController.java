a2043588abf69f5943bd722b317a17f2a985b8df
Henderson Silva <hendersonsilva_@hotmail.com>
2026-07-16 15:00:38 -0300

    quick fix

--- src/main/java/com/henderson/catalog/controller/CategoryController.java
+++ src/main/java/com/henderson/catalog/controller/CategoryController.java
@@ -1,23 +1,27 @@
 package com.henderson.catalog.controller;
 
-import com.henderson.catalog.entities.Category;
+import com.henderson.catalog.dto.CategoryDTO;
+import com.henderson.catalog.service.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryController {
 
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L, "Books"));
-        list.add(new Category(2L, "Eletronics"));
-        return ResponseEntity.ok().body(list);
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> list = categoryService.findAll();
+        return ResponseEntity.ok(list);
     }
 }
