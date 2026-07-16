350788a52fa66fd7a3a8b3a3917af9d05fe4ad8b
Henderson Silva <hendersonsilva_@hotmail.com>
2026-07-16 16:03:48 -0300

    feat: ResourceNotFoundException

---
+++ src/main/java/com/henderson/catalog/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.henderson.catalog.service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
