3074fa0ca49157f68bd903b251ced9709f155500
Henderson Silva <hendersonsilva_@hotmail.com>
2026-07-16 16:01:49 -0300

    refactor CategoryService

--- src/main/java/com/henderson/catalog/service/CategoryService.java
+++ src/main/java/com/henderson/catalog/service/CategoryService.java
@@ -3,10 +3,12 @@ package com.henderson.catalog.service;
 import com.henderson.catalog.dto.CategoryDTO;
 import com.henderson.catalog.entity.Category;
 import com.henderson.catalog.repository.CategoryRepository;
+import com.henderson.catalog.service.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -22,4 +24,13 @@ public class CategoryService {
         List<Category> result = categoryRepository.findAll();
         return result.stream().map(x -> new CategoryDTO(x)).toList();
     }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
+
+
 }
