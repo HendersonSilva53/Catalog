a9112e8459f041d716e6816a2b17df5177c2c900
Henderson Silva <hendersonsilva_@hotmail.com>
2026-07-16 15:01:47 -0300

    Category DTO

---
+++ src/main/java/com/henderson/catalog/dto/CategoryDTO.java
@@ -0,0 +1,30 @@
+package com.henderson.catalog.dto;
+
+import com.henderson.catalog.entity.Category;
+
+public class CategoryDTO {
+
+    private Long id;
+
+    private String name;
+
+    public CategoryDTO(){}
+
+    public CategoryDTO(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO(Category entity){
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
