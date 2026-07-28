f1c7b74c9172301ab2344864afc96d19522065fb
Henderson Silva <hendersonsilva_@hotmail.com>
2026-07-28 15:06:28 -0300

    feat: ProductRepository

---
+++ src/main/java/com/henderson/catalog/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.henderson.catalog.repository;
+
+import com.henderson.catalog.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
